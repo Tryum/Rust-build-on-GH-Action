a06869da85180a9125309ccd9178d650fa5636a2
Thibault Jochem <161625+Tryum@users.noreply.github.com>
2024-02-16 09:52:05 +0100

    fix ps1 script

--- .scripts/build.ps1
+++ .scripts/build.ps1
@@ -3,7 +3,7 @@
 Write-Host "Building with rust channel : $env:rust_channel"
 
 $build_channel = ""
-if ($env:rust_channel == "nighly") {
+if ($env:rust_channel -eq "nighly") {
     $build_channel = "+nightly"
 }
 
